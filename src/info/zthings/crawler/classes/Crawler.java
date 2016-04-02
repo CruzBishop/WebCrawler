62609493769e5ad840da000aaf92b611c41ba83a
Ravi van Rooijen <github@ravithrasworld.nl>
2016-04-02 12:55:06 +0200

    Using jsoup to scrape all the links

--- src/info/zthings/crawler/classes/Crawler.java
+++ src/info/zthings/crawler/classes/Crawler.java
@@ -2,29 +2,37 @@ package info.zthings.crawler.classes;
 
 import info.zthings.crawler.common.Ref;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Calendar;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 public class Crawler {
 	//TODO: dual-stream with ConsoleUI & logfile?
 	private PrintStream log;
 	private final String BASE_LOCATION = "crawls/date-" + Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + "-" + Calendar.getInstance().get(Calendar.MONTH) + "-" + Calendar.getInstance().get(Calendar.YEAR) + "/time-" + Calendar.getInstance().get(Calendar.HOUR_OF_DAY) + "-" + Calendar.getInstance().get(Calendar.MINUTE) + "-" + Calendar.getInstance().get(Calendar.SECOND) + "/";	private String loc;
+	private String n;
 	
 	public Crawler(String httploc) {
 		this.loc = httploc;
+		this.n = "start.log";
+	}
+	public Crawler(String httploc, String name) {
+		this.loc = httploc;
+		this.n = name;
 	}
 
 	public void start() {
 		try {
 			new File(BASE_LOCATION).mkdirs();
-			File f = new File(BASE_LOCATION + "start.log");
+			File f = new File(BASE_LOCATION + this.n);
 			f.createNewFile();
 			log = new PrintStream(f);
 		} catch (IOException e) {
@@ -32,22 +40,19 @@ public class Crawler {
 		}
 
 		log.println("CRAWLING AT "+loc);
-		log.println(Ref.SEP);
-		log.println("Requesting page body...");
+		log.println(Ref.SEP.substring(0, 12+loc.length()));
+		log.println("Requesting document object...");
 		
 		try {
-			URL oracle = new URL(loc);
-			BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+			Document d = Jsoup.connect(loc).get();
+			Elements links = d.select("a[href]");
 			
-			String p = BASE_LOCATION + loc.substring(11).replace('.', '-');
-			if (p.charAt(p.length()-1) == '/') p = p.substring(0, p.length()-1);
-			BodyBuffer bBuffer = new BodyBuffer(p);
-			String cl;
-			while ((cl = in.readLine()) != null) bBuffer.println(cl);
-			in.close();
+			for (Element e : links) {
+				log.println("Found link: " + e.attr("href"));
+			}
 		} catch (MalformedURLException e) { 
 			log.println("URL is invalid for Java: " + loc);
-			e.printStackTrace();
+			e.printStackTrace(log);
 		} catch (IOException e) {
 			log.println("Other IO exception occured");
 			e.printStackTrace();
