a2b40d8b29449082011841170003f6992362e090
Ravi van Rooijen <github@ravithrasworld.nl>
2016-04-10 22:22:22 +0200

    v0.1.1-alpha

--- src/info/zthings/crawler/common/Ref.java
+++ src/info/zthings/crawler/common/Ref.java
@@ -1,7 +1,7 @@
 package info.zthings.crawler.common;
 
 public class Ref {
-	public static final String VER = "0.1.0";
+	public static final String VER = "0.1.1";
 	
 	public static String sep() {
 		return "=====================";
